297e836fee3d9b6a54b0e0dc9fc076592240aeb6
vaishnavi <vaishuirc01@gmail.com>
2025-12-01 12:10:57 +0530

    ok

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import todosRouter from "./routes/todos.js";
+import todosRouter from "./routes/todos";
 
 dotenv.config();
 
